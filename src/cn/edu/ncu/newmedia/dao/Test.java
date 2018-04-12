187dfa1c0bc78064b713912650f0425a3fe5551b
ananotherworld <861803342@qq.com>
2018-04-12 22:45:54 +0800

    Add a new feature
    Add a new feature
    
    Add anew feature

---
+++ src/cn/edu/ncu/newmedia/dao/Test.java
@@ -0,0 +1,17 @@
+package cn.edu.ncu.newmedia.dao;
+
+import cn.edu.ncu.newmedia.bean.Recruit;
+
+public class Test {
+    public static void main(String []args) throws Exception {
+        RecruitDao recruitDao = new RecruitDao();
+        Recruit recruit = new Recruit();
+        recruit = recruitDao.get(9,4);
+        int total = recruitDao.gettotal(4);
+        System.out.println(total);
+        String recruitStr = "\"recruit" +  "\"" + ":" + "[" ;
+        recruitStr += "{" + "\"id"+"\""+":"+"\""+ recruit.getId()+"\"," + "\"communityId"+"\""+":"+"\""+recruit.getCommunityId()+"\"," + "\"name"+"\""+":"+"\""+recruit.getName()+"\"," + "\"sex"+"\""+":"+"\""+recruit.getSex()+"\"," + "\"nation"+"\""+":"+"\""+recruit.getNation()+"\"," + "\"birthday"+"\""+":"+"\""+ recruit.getBirthday() +"\"," + "\"nativePlace"+"\""+":"+"\""+ recruit.getNativePlace() +"\"," + "\"politicalAffiliation"+"\""+":"+"\""+ recruit.getPoliticalAffiliation() +"\","  + "\"college"+"\""+":"+"\""+ recruit.getCollege() +"\"," + "\"major"+"\""+":"+"\""+ recruit.getMajor() +"\"," + "\"phone"+"\""+":"+"\""+ recruit.getPhone() +"\"," + "\"department1"+"\""+":"+"\""+ recruit.getDepartment1() +"\"," + "\"department2"+"\""+":"+"\""+ recruit.getDepartment2() +"\"," + "\"department3"+"\""+":"+"\""+ recruit.getDepartment3() +"\"," + "\"speciality"+"\""+":"+"\""+ recruit.getSpeciality() +"\"," + "\"experience"+"\""+":"+"\""+ recruit.getExperience() +"\"," + "\"timesId"+"\""+":"+"\""+ recruit.getTimesId() +"\"," + "\"messageState"+"\""+":"+"\""+ recruit.getMessageState() +"\"," + "\"createAt"+"\""+":"+"\""+ recruit.getCreateAt() +"\"," + "\"state"+"\""+":"+"\""+ recruit.getState() +"\"" + "}," ;
+        recruitStr = "{"+recruitStr.substring(0, recruitStr.length()-1)+"]}" ;
+        System.out.println(recruitStr);
+    }
+}
